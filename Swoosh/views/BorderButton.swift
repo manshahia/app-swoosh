f010bc7514eff6740b3dc0a0261ef823c0f3ce70
Ravi Inder Manshahia <ravinder@lab91.co>
2019-02-05 19:43:39 +0530

    Created Welcome Screen. Part 1/10

---
+++ Swoosh/views/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  Swoosh
+//
+//  Created by Ravi Inder Manshahia on 05/02/19.
+//  Copyright © 2019 Ravi Inder Manshahia. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
