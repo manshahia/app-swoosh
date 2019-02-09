fe20732f97a46d71102ab95ff3a5904a705743b6
Ravi Inder Manshahia <ravinder@lab91.co>
2019-02-09 14:11:16 +0530

    Refactoring & Added VC files

---
+++ Swoosh/controller/LeagueVC.swift
@@ -0,0 +1,35 @@
+//
+//  LeagueVC.swift
+//  Swoosh
+//
+//  Created by Ravi Inder Manshahia on 09/02/19.
+//  Copyright © 2019 Ravi Inder Manshahia. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
